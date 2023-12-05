408dfea2478adec5a32f2d5c2580012545316d48
lavabyte <buweiliao@gmail.com>
2023-12-05 14:03:25 +0100

    Added nfts table

--- cloudflare_worker/schema/init.sql
+++ cloudflare_worker/schema/init.sql
@@ -1,3 +1,7 @@
+-- Note:
+-- 1. Every SQL statement must end with a semicolon (;)
+-- 2. No (,) at the end of the last statement
+
 drop table if exists "profiles";
 
 create table "profiles" (
@@ -7,6 +11,15 @@ create table "profiles" (
   "birth_date" date,
   "created_at" datetime default current_timestamp,
   "updated_at" datetime default current_timestamp
+);
+
+create table "nfts" (
+  "id" integer primary key autoincrement,
+  "profile_id" integer not null,
+  "hash" text not null,
+  “transaction_id” text not null,
+  "log" json not null,
+  "created_at" datetime default current_timestamp
 );
 
 insert into "profiles" ("name", "user_id") values ('Super Pro', 'demo');
