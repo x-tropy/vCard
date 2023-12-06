bf25e88ade91c9b9c4e2c9d34be426c48cc92b2a
lavabyte <buweiliao@gmail.com>
2023-12-07 00:08:30 +0100

    Implemented zod validation

--- app/[slug]/general/page.js
+++ app/[slug]/general/page.js
@@ -9,12 +9,10 @@ export default async function Page({ params: { slug } }) {
   // Hanlde error
   if (result.status == 'error') return <h1>{result.message}</h1>;
 
-  const { id, user_id, created_at, name } = result.data;
-
   return (
     <div className="space-y-6">
       <TabPanelTitle title="General" description="General settings for your profile." />
-      <TabGeneral id={id} user_id={user_id} created_at={created_at} name={name} />
+      <TabGeneral data={result.data} />
     </div>
   );
 }
